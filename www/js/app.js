440cb439a1fda53582f6681c41059e78d748afb7
Mike Cooper <mythmon@gmail.com>
2012-10-25 12:05:51 -0700

    Initial Calculator Functions.
    Stack control, add, subtract, multiply, divide, numbers, positive and
    negative.

--- www/js/app.js
+++ www/js/app.js
@@ -1,83 +1,140 @@
-
-// The code below uses require.js, a module system for javscript:
-// http://requirejs.org/docs/api.html#define
-
-require.config({ 
-    baseUrl: 'js/lib',
-    paths: {'jquery':
-            ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-             'jquery']},
+require.config({
+  baseUrl: 'js/lib',
+  paths: {'jquery':
+      ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
+       'jquery']},
 
 });
-
-// Include the in-app payments API, and if it fails to load handle it
-// gracefully.
-// https://developer.mozilla.org/en/Apps/In-app_payments
-require(['https://marketplace.cdn.mozilla.net/mozmarket.js'],
-        function() {},
-        function(err) {
-            window.mozmarket = window.mozmarket || {};
-            window.mozmarket.buy = function() {
-                alert('The in-app purchasing is currently unavailable.');
-            };
-        });
-
-
 
 // When you write javascript in separate files, list them as
 // dependencies along with jquery
 define("app", function(require) {
 
-    var $ = require('jquery');
+  var $ = require('jquery');
 
-    // If using Twitter Bootstrap, you need to require all the
-    // components that you use, like so:
-    // require('bootstrap/dropdown');
-    // require('bootstrap/alert');
+  $('body').on('click', 'button', function() {
+    var $this = $(this);
+    var id = $this.prop('id');
+    console.log(id);
 
-
-    // START HERE: Put your js code here
-
-
-
-
-
-    // Hook up the installation button, feel free to customize how
-    // this works
-    
-    var install = require('install');
-
-    function updateInstallButton() {
-        $(function() {
-            var btn = $('.install-btn');
-            if(install.state == 'uninstalled') {
-                btn.show();
-            }
-            else if(install.state == 'installed' || install.state == 'unsupported') {
-                btn.hide();
-            }
-        });
+    if ($this.hasClass('number')) {
+      var digit = parseFloat(id);
+      if (Stack.progress) {
+        var num = Stack.pop();
+        if (Stack.decimal) {
+          num = num + '';
+          if (num.indexOf('.') == -1) {
+            num += '.';
+          }
+          num += '' + digit;
+          num = parseFloat(num);
+          Stack.push(num);
+        } else {
+          num *= 10;
+          num += digit;
+          Stack.push(num);
+        }
+      } else {
+        Stack.push(digit);
+        Stack.progress = true;
+      }
     }
-
-    $(function() {
-        $('.install-btn').click(install);        
-    });
-
-    install.on('change', updateInstallButton);
-
-    install.on('error', function(e, err) {
-        // Feel free to customize this
-        $('.install-error').text(err.toString()).show();
-    });
-
-    install.on('showiOSInstall', function() {
-        // Feel free to customize this
-        var msg = $('.install-ios-msg');
-        msg.show();
-        
-        setTimeout(function() {
-            msg.hide();
-        }, 8000);
-    });
-
+    else if ($this.hasClass('operator')) {
+      var op = Operators[id];
+      if (op === undefined) {
+        flash('error', 'Undefined operator ' + id);
+      }
+      console.log(op);
+      op();
+    }
+    else if ($this.hasClass('action')) {
+      if (id == 'enter') {
+        Stack.decimal = false;
+        Stack.progress = true;
+        Stack.push(0);
+      }
+      else if (id == 'decimal') {
+        Stack.decimal = true;
+        Stack.progress = true;
+        Stack.redraw();
+      }
+    } else {
+      flash('error', 'wat');
+    }
+  });
 });
+
+function flash(level, message) {
+  var $msg = $('<span>' + message + '</span>')
+    .appendTo('.screen .messages');
+
+  setTimeout(function() {
+    $msg.fadeOut(1000);
+  }, 3000);
+}
+
+function safe_op(n, op) {
+  return function() {
+    if (Stack.data.length < n) {
+      flash('error', 'Not enough parameters.');
+    } else {
+      var args = [];
+      for (var i = 0; i < n; i++) {
+        // Insert in reverse order.
+        args.splice(0, 0, Stack.pop());
+      }
+
+      var ret = op.apply(this, args);
+      if (ret instanceof Array) {
+        for (var i = 0; i < ret.length; i++) {
+          Stack.push.apply(this, ret[i]);
+        }
+      } else if (ret !== undefined) {
+        Stack.push(ret);
+      }
+
+      Stack.progress = false;
+      Stack.decimal = false;
+    }
+  }
+}
+
+Operators = {
+  add: safe_op(2, function(a, b) { return a + b; }),
+  sub: safe_op(2, function(a, b) { return a - b; }),
+  mul: safe_op(2, function(a, b) { return a * b; }),
+  div: safe_op(2, function(a, b) { return a / b; }),
+  neg: safe_op(1, function(a) { return -a; }),
+ 
+  drop: safe_op(1, function(a) { return undefined; }),
+  swap: safe_op(1, function(a, b) { return [a, b]; }),
+}
+
+Stack = {
+  data: [],
+  progress: false,
+  decimal: false,
+  push: function(n) {
+    this.data.push(n );
+    this.redraw();
+  },
+  pop: function() {
+    var n = this.data.pop();
+    this.redraw();
+    return n;
+  },
+  redraw: function() {
+    var disp = "";
+    for (var i = 0; i < this.data.length; i++) {
+      var num = this.data[i];
+      if (this.decimal && i == this.data.length - 1) {
+        num = num + '';
+        if (num.indexOf('.') == -1) {
+          num += '.';
+        }
+      }
+      disp += '<span class="row">' + num + '</span>';
+    }
+    $('#main-screen .stack').html(disp);
+  }
+}
